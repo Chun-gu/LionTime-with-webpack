8a72491fe82a592292811bae1fede77c891960aa
Chun-gu <chungu.yi92@gmail.com>
2022-10-29 03:13:37 +0900

    🩹 쿼리스트링 수정

--- js/footer.js
+++ js/footer.js
@@ -3,5 +3,5 @@ const FOOTER_ACCOUNTNAME = sessionStorage.getItem('my-accountname');
 const profileLink = document.querySelector('.link-other-page.profile');
 profileLink.addEventListener('click', (e) => {
     e.preventDefault();
-    location.href = `profile.html?${FOOTER_ACCOUNTNAME}`;
+    location.href = `profile.html?userId=${FOOTER_ACCOUNTNAME}`;
 });
