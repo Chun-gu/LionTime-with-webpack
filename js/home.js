768de3318c3260510b25be9ff481df962c4781d3
Chun-gu <chungu.yi92@gmail.com>
2022-10-30 23:58:57 +0900

    ♻️  home 페이지 피드 DOM 구조 개선

--- js/home.js
+++ js/home.js
@@ -1,28 +1,21 @@
-import { API_URL } from './key.js';
+import { API_URL, IMAGE_URL } from './key.js';
+import { trimImageURL } from './lib.js';
 const TOKEN = sessionStorage.getItem('my-token');
 
-async function getFeed() {
-    const res = await fetch(API_URL + '/post/feed', {
-        //메소드 구분
-        method: 'GET',
-        //헤더
-        headers: {
-            Authorization: `Bearer ${TOKEN}`,
-            'Content-type': 'application/json',
-        },
-    });
-    const json = await res.json(); //외않됌? 포인트 res.json()도 비동기. await을 해줘야한다.
-    return json;
-}
+const LIMIT = 5;
+let skip = 0;
+
+printFeed();
 
 async function printFeed() {
-    const feedData = await getFeed();
-    if (feedData.posts.length > 0) {
-        const noFeed = document.querySelector('.noFeed');
-        noFeed.remove();
+    const { posts } = await getFeed();
+
+    if (posts.length === 0) {
+        printNoFeed();
+        return;
     }
-    const postContainer = document.querySelector('.post');
-    for (const feed of feedData.posts) {
+
+    for (const post of posts) {
         const {
             id,
             author: { image: authorImage, username, accountname },
@@ -32,67 +25,117 @@ async function printFeed() {
             hearted,
             commentCount,
             createdAt,
-        } = feed;
-        postContainer.innerHTML += `
-        <article class="postCont">
-            <a href="#">
-                <img src=${authorImage} alt="user profile image" onerror="this.src='../images/default-profile-img.png'" class="usrImg" />
+        } = post;
+        feedList.innerHTML += `
+        <li class="post-card">
+            <a class="author-image" href="../pages/profile.html?userId=${accountname}">
+                <img src=${trimImageURL(
+                    authorImage
+                )} alt="${accountname}" onerror="this.src='../images/default-profile-img.png'"/>
             </a>
-            <div class="postOne">
-                <h3>
-                    <a href="../pages/profile.html?${accountname}" class="usrName">${username}</a>
-                </h3>
-                <a class="usrSubName">@ ${accountname}</a>
-                <p class="postTxt" data-post-id=${id}>
+            <div class="post-content">
+                <a class="author-profile">
+                    <span class="user-name">${username}</span>
+                    <span class="account-name">@ ${accountname}</span>
+                </a>
+                <p class="post-text">
                     ${content}
                 </p>
-                <img src=${image} alt="post image" onerror="this.src='../images/default-post-product-image.png'" class="postImg" data-post-id=${id}/>
+                <a href="../pages/post.html?postId=${id}">
+                    <img src=${trimImageURL(
+                        image
+                    )} alt="post image" onerror="this.src='../images/default-post-product-image.png'" class="post-image" />
+                </a>
                 <div class="reaction">
                     <button type="button" class="like" data-hearted=${hearted}></button>
                     <span class="count heart-count">${heartCount}</span>
-                    <a href= "../pages/post.html?${id}">
-                        <button class="commentBtn" type="button" data-post-id=${id}>
-                            <img src="../images/icon-message-small.png" class="commentIcon" />
+                    <a href= "../pages/post.html?postId=${id}">
+                        <button class="comment-btn" type="button" data-post-id=${id}>
+                            <img src="../images/icon-message-small.png" class="comment-icon" />
                         </button>
                         <span class="count comment-count">${commentCount}</span>
                     </a>
                 </div>
-                <small class="postDate">
+                <small class="post-date">
                     ${createdAt.slice(0, 4)}년 
                     ${createdAt.slice(5, 7)}월 
                     ${createdAt.slice(8, 10)}일 
                 </small>
-            </div> 
-        </article>
+            </div>
+        </li>
         `;
     }
 }
 
-const goPostPage = document.querySelectorAll('.commentBtn');
-for (const [idx, comment] of goPostPage.entries()) {
-    comment.addEventListener('click', () => {
-        window.location.href = `post.html?${posts[idx].id}`;
-    });
-}
+async function getFeed() {
+    try {
+        const res = await fetch(
+            `${API_URL}/post/feed?limit=${LIMIT}&skip=${skip}`,
+            {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${TOKEN}`,
+                    'Content-type': 'application/json',
+                },
+            }
+        );
+        const data = await res.json();
 
-getFeed();
-
-printFeed();
-const postContainer = document.querySelector('.post');
-postContainer.addEventListener('click', (e) => {
-    if (e.target.classList.contains('like')) {
-        likePost(e.target);
-        return;
-    }
-});
-function likePost(target) {
-    const isHearted = target.dataset.hearted;
-    const likeCount = target.nextSibling.nextSibling;
-    if (isHearted === 'true') {
-        target.dataset.hearted = false;
-        likeCount.textContent -= 1;
-    } else {
-        target.dataset.hearted = true;
-        likeCount.textContent = +likeCount.textContent + 1;
+        return data;
+    } catch (error) {
+        console.log(error);
+        alert('피드를 가져오는 데에 실패했습니다.');
     }
 }
+
+const feedList = document.querySelector('.feed-list');
+function printNoFeed() {
+    const li = document.createElement('li');
+    li.classList.add('no-feed');
+
+    const img = document.createElement('img');
+    img.classList.add('logo');
+    img.src = '../images/logo-lion-grey.png';
+    img.setAttribute('alt', '라이언 타임 로고');
+
+    const span = document.createElement('span');
+    span.classList.add('follow-suggestion');
+    span.textContent = '유저를 검색해 팔로우 해보세요!';
+
+    const a = document.createElement('a');
+    a.classList.add('searchBtn');
+    a.href = '../pages/search.html';
+    a.textContent = '검색하기';
+
+    li.append(img);
+    li.append(span);
+    li.append(a);
+    feedList.append(li);
+}
+
+// const goPostPage = document.querySelectorAll('.commentBtn');
+// for (const [idx, comment] of goPostPage.entries()) {
+//     comment.addEventListener('click', () => {
+//         window.location.href = `post.html?postId=${posts[idx].id}`;
+//     });
+// }
+
+// const postContainer = document.querySelector('.post');
+// postContainer.addEventListener('click', (e) => {
+//     if (e.target.classList.contains('like')) {
+//         likePost(e.target);
+//         return;
+//     }
+// });
+
+// function likePost(target) {
+//     const isHearted = target.dataset.hearted;
+//     const likeCount = target.nextSibling.nextSibling;
+//     if (isHearted === 'true') {
+//         target.dataset.hearted = false;
+//         likeCount.textContent -= 1;
+//     } else {
+//         target.dataset.hearted = true;
+//         likeCount.textContent = +likeCount.textContent + 1;
+//     }
+// }
