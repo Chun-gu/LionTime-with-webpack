12b974f8c758a0ca62f4b9da3cd05664d9935fc8
Chun-gu <chungu.yi92@gmail.com>
2022-11-03 00:06:32 +0900

    ✨ 이미지URL, 디바운스, 쿼리스트링 관련 유틸 함수 구현

--- js/lib.js
+++ js/lib.js
@@ -1,7 +1,27 @@
-import { IMAGE_URL } from './key';
+import { IMAGE_URL } from './key.js';
 
+// IMAGE_URL이 없으면 붙여주고, 중복되면 중복 제거
 export function trimImageURL(url) {
-    const URLPattern = new RegExp(IMAGE_URL, 'g');
-    if (URLPattern.test(url)) return url;
+    const URLPattern = new RegExp(
+        `(${IMAGE_URL.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')})+`
+    );
+    if (URLPattern.test(url)) return url.replace(URLPattern, IMAGE_URL);
     return IMAGE_URL + url;
+}
+
+// 디바운스
+export function debounce(func, timeout) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => func.apply(this, args), timeout);
+    };
+}
+
+// queryString에서 원하는 값 가져오기
+export function getFromQueryString(key) {
+    const queryString = new URLSearchParams(location.search);
+    const value = queryString.get(key);
+
+    return value;
 }
