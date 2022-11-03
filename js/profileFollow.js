1bc93c6014958ba1e89d44194fd57a8153e187b9
Chun-gu <chungu.yi92@gmail.com>
2022-11-03 23:51:12 +0900

    ♻️ 팔로우 언팔로우 기능 분기와 에러 핸들

--- js/profileFollow.js
+++ js/profileFollow.js
@@ -1,9 +1,10 @@
 import { API_URL } from './key.js';
+import { getFromQueryString, trimImageURL } from './lib.js';
+
 const MY_ID = sessionStorage.getItem('my-id');
 const MY_ACCOUNTNAME = sessionStorage.getItem('my-accountname');
 const TOKEN = sessionStorage.getItem('my-token');
-const TARGET_ACCOUNTNAME = location.href.split('?')[1];
-const isMyProfile = MY_ACCOUNTNAME === TARGET_ACCOUNTNAME;
+const TARGET_ACCOUNTNAME = getFromQueryString('userId');
 
 const isFollowersPage = localStorage.getItem('is-followers-page') === 'true';
 localStorage.removeItem('is-followers-page');
@@ -17,32 +18,10 @@ if (isFollowersPage) {
     pageName.textContent = 'Followings';
 }
 
-// 로그인
-// (async function login() {
-//     try {
-//         const res = await fetch(API_URL + 'user/login', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({
-//                 user: {
-//                     email: 'testEmail@test.com',
-//                     password: 'testpassword',
-//                 },
-//             }),
-//         });
-//         const resJson = await res.json();
-//         const { _id, accountname, token } = resJson.user;
-//         sessionStorage.setItem('token', token);
-//         sessionStorage.setItem('my-accountname', accountname);
-//         sessionStorage.setItem('my-id', _id);
-//     } catch (err) {}
-// })();
-
 const usersList = document.querySelector('.users-list');
 const limit = 15;
 let skip = 0;
+
 async function fetchData(endpoint) {
     try {
         const res = await fetch(
@@ -58,9 +37,10 @@ async function fetchData(endpoint) {
         );
         skip += limit;
         const resJson = await res.json();
+
         return resJson;
     } catch (err) {
-        console.log(err);
+        alert('정보를 가져오는 데에 실패했습니다.');
     }
 }
 
@@ -73,40 +53,46 @@ async function getFollowsData() {
 
 function makeFollowsItem(follows) {
     const { accountname, image, username, intro, follower } = follows;
+
     const followsItem = document.createElement('li');
     followsItem.classList.add('users-list-item');
     followsItem.dataset.accountname = accountname;
+
     const img = document.createElement('img');
     img.classList.add('user-img');
-    if (image.slice(0, 4) === 'http') {
-        img.setAttribute('src', image);
-    } else {
-        img.setAttribute('src', API_URL + image);
-    }
+    img.setAttribute('src', trimImageURL(image));
     img.setAttribute(
         'onError',
         "this.src='https://mandarin.api.weniv.co.kr/Ellipse.png'"
     );
+
     const div = document.createElement('div');
     div.classList.add('user-profile');
+
     const span = document.createElement('span');
     span.classList.add('user-name');
     span.textContent = username;
     div.append(span);
+
     if (!!intro) {
         const p = document.createElement('p');
         p.classList.add('user-intro');
         p.textContent = intro;
         div.append(p);
     }
-    const button = document.createElement('button');
-    button.classList.add('btn-follow');
-    if (follower.includes(MY_ID)) {
-        button.classList.add('cancel');
-    }
+
     followsItem.append(img);
     followsItem.append(div);
-    followsItem.append(button);
+
+    if (accountname !== MY_ACCOUNTNAME) {
+        const button = document.createElement('button');
+        button.classList.add('btn-follow');
+        if (follower.includes(MY_ID)) {
+            button.classList.add('cancel');
+        }
+        followsItem.append(button);
+    }
+
     return followsItem;
 }
 
@@ -153,9 +139,10 @@ function observeLastItem(followsIo, items) {
 })();
 
 async function toggleFollow(accountname, endpoint, method) {
+    let isSuccess = false;
     try {
         const res = await fetch(
-            API_URL + `profile/${accountname}/${endpoint}`,
+            `${API_URL}/profile/${accountname}/${endpoint}`,
             {
                 method: method,
                 headers: {
@@ -164,23 +151,35 @@ async function toggleFollow(accountname, endpoint, method) {
                 },
             }
         );
-        console.log(await res.json());
+        const { profile } = await res.json();
+
+        if (profile) isSuccess = true;
+        else alert('오류가 발생했습니다.');
     } catch (error) {
-        console.log(error);
+        alert('오류가 발생했습니다.');
     }
+    return isSuccess;
 }
 
 // 기능 분기
-usersList.addEventListener('click', ({ target }) => {
+usersList.addEventListener('click', async ({ target }) => {
     const target_accountname = target.closest('li').dataset.accountname;
     if (target.classList.contains('cancel')) {
-        target.classList.toggle('cancel');
-        toggleFollow(target_accountname, 'unfollow', 'DELETE');
+        const isSuccess = await toggleFollow(
+            target_accountname,
+            'unfollow',
+            'DELETE'
+        );
+        if (isSuccess) target.classList.toggle('cancel');
         return;
     }
     if (target.classList.contains('btn-follow')) {
-        target.classList.toggle('cancel');
-        toggleFollow(target_accountname, 'follow', 'POST');
+        const isSuccess = await toggleFollow(
+            target_accountname,
+            'follow',
+            'POST'
+        );
+        if (isSuccess) target.classList.toggle('cancel');
         return;
     }
     if (
@@ -188,7 +187,7 @@ usersList.addEventListener('click', ({ target }) => {
         target.classList.contains('user-name') ||
         target.classList.contains('user-intro')
     ) {
-        location.href = `../pages/profile.html?${target_accountname}`;
+        location.href = `../pages/profile.html?userId=${target_accountname}`;
         return;
     }
 });
