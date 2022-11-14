15d48badc2419cdd635991e478ccf30fffc43ff4
Chun-gu <chungu.yi92@gmail.com>
2022-11-14 23:04:15 +0900

    ✨ 토큰, 상대방 id, dom 요소 할당

--- js/chatRoom.js
+++ js/chatRoom.js
@@ -1,5 +1,22 @@
-const messageSendBtn = document.querySelector('.btn-message-send');
-const inputText = document.querySelector('.inp-chat');
+import { API_URL } from './key.js';
+import { getFromQueryString, trimImageURL } from './lib.js';
+
+const TOKEN = sessionStorage.getItem('my-token');
+const partnerId = getFromQueryString('userId');
+
+const messageSection = document.querySelector('.message-section');
+const imageInput = document.querySelector('#image-attach-input');
+const inputContainer = document.querySelector('.input-container');
+const messageTextarea = document.querySelector('#message-textarea');
+const messageForm = document.querySelector('#message-form');
+const sendButton = document.querySelector('#send-button');
+
+document.addEventListener('DOMContentLoaded', scrollToBottom);
+
+function scrollToBottom() {
+    window.scrollTo(0, document.body.scrollHeight);
+}
+
 const backKeyBtn = document.querySelector('.btn-back-key');
 const currentTime = document.querySelector('.text-current-time');
 
