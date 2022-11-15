256bda641e2ffb2a07c0679172e42b8cfff8765f
Chun-gu <chungu.yi92@gmail.com>
2022-11-15 16:08:51 +0900

    ⚰️ 불필요한 코드 삭제

--- js/chatList.js
+++ js/chatList.js
@@ -1,25 +1,5 @@
-const linkImgBtn = document.querySelectorAll('.link-img');
 const backKeyBtn = document.querySelector('.btn-back-key');
-const currentTime = document.querySelector('.text-current-time');
-linkImgBtn.forEach((selectBtn) => {
-    selectBtn.addEventListener('click', () => {
-        selectBtn.classList.add('this-page');
-        linkImgBtn.forEach((btn) => {
-            if (btn !== selectBtn) {
-                btn.classList.remove('this-page');
-            }
-        });
-    });
-});
 
 backKeyBtn.addEventListener('click', () => {
     history.back();
 });
-
-function currentTimer() {
-    let date = new Date();
-    currentTime.innerText = `${date.getHours()} : ${
-        date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
-    } ${date.getHours() >= 12 ? `PM` : `AM`}`;
-}
-setInterval(currentTimer, 1000);
