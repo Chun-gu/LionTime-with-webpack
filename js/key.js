7fab36041f35c635b78eafa156c0fb48cb426b62
Chun-gu <chungu.yi92@gmail.com>
2022-12-10 14:30:12 +0900

    🔥 불필요한 images, 환경변수 삭제

--- js/key.js
+++
@@ -1,4 +0,0 @@
-// export const API_URL = 'http://localhost:8888';
-export const API_URL = 'https://mandarin.api.weniv.co.kr';
-// export const API_URL = 'https://liontime-api-redirect.netlify.app';
-export const IMAGE_URL = 'https://mandarin.api.weniv.co.kr/';
