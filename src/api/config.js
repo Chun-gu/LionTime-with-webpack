965effa0949905d57aafba80bab38a25ae2c61bd
Chun-gu <chungu.yi92@gmail.com>
2022-12-13 18:06:27 +0900

    ✨ api 메소드, 옵션에 따른 분리

---
+++ src/api/config.js
@@ -0,0 +1,35 @@
+import { HTTPError } from '@components';
+
+const API_URL = process.env.API_URL;
+const TOKEN = sessionStorage.getItem('my-token');
+
+function init(method, body) {
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${TOKEN}`,
+  };
+
+  if (body instanceof FormData) delete headers['Content-Type'];
+  if (!(body instanceof FormData)) body = JSON.stringify(body);
+
+  return {
+    method,
+    headers,
+    body,
+  };
+}
+
+const api = {
+  post: async (url, body) => {
+    try {
+      const response = await fetch(API_URL + url, init('POST', body));
+
+      if (response.ok) return await response.json();
+      else throw await response.json();
+    } catch (error) {
+      throw new HTTPError(error);
+    }
+  },
+};
+
+export default api;
