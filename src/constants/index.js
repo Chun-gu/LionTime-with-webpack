01ca23677495298c76c5a7ca934dde2d02e87522
Chun-gu <chungu.yi92@gmail.com>
2022-12-13 18:10:11 +0900

    ✨ 리터럴, 정규식 상수화

---
+++ src/constants/index.js
@@ -0,0 +1,17 @@
+export const PAGE = {
+  profileSetting: 'profileSetting',
+};
+
+export const EMAIL = {
+  wrongPattern: '이메일 형식이 아닙니다.',
+  available: '사용 가능한 이메일 입니다.',
+};
+
+export const PASSWORD = {
+  minLength: '비밀번호는 6자 이상이어야 합니다',
+  maxLength: '비밀번호는 16자 이하이어야 합니다',
+};
+
+export const REGEX = {
+  email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+};
