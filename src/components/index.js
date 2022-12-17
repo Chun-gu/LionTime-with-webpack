6a84c1e3a2c75dfd2977efcd1c213cebe639818e
Chun-gu <chungu.yi92@gmail.com>
2022-12-17 11:05:10 +0900

    ✨ 컴포넌트 export

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
+export { default as BottomSheet } from './BottomSheet';
 export * from './CustomError';
+export { default as NoFeed } from './NoFeed';
+export * from './PostItem';
 export { default as StatusBar } from './StatusBar';
