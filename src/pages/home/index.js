28fd1256442ef65abc29fef2229e71d9a5e073e9
Chun-gu <chungu.yi92@gmail.com>
2022-12-17 11:12:58 +0900

    ✨ 홈 페이지

---
+++ src/pages/home/index.js
@@ -0,0 +1,81 @@
+import './style.css';
+
+import { getFeed, heartPost, unheartPost } from '@api';
+import { BottomSheet, PostItem, NoFeed, StatusBar } from '@components';
+import { intersectionObserver } from '@utils';
+
+const MY_ACCOUNTNAME = sessionStorage.getItem('my-accountname');
+
+const LIMIT = 5;
+let skip = 0;
+
+const feedList = document.querySelector('.feed-list');
+const feedListObserver = intersectionObserver(feedList);
+
+StatusBar();
+printFeed();
+
+feedList.addEventListener('intersect', printFeed);
+
+feedList.addEventListener('click', async ({ target }) => {
+  const targetClassList = target.classList;
+
+  if (targetClassList.contains('post-menu-button')) {
+    const postId = target.closest('.post-card').dataset.postId;
+    const isMine =
+      target.closest('.post-card').dataset.author === MY_ACCOUNTNAME;
+
+    new BottomSheet({ type: 'post', postId, isMine }).open();
+  }
+
+  if (targetClassList.contains('heart-button')) {
+    const postId = target.closest('.post-card').dataset.postId;
+    const heartCount = target.nextElementSibling;
+    const isHearted = JSON.parse(target.dataset.hearted);
+
+    const { ok, error } = isHearted
+      ? await unheartPost(postId)
+      : await heartPost(postId);
+
+    if (ok) {
+      target.dataset.hearted = !isHearted;
+      heartCount.textContent =
+        parseInt(heartCount.textContent) + (isHearted ? -1 : 1);
+    } else {
+      alert(error);
+    }
+  }
+});
+
+async function printFeed() {
+  const { ok, posts, error } = await getFeed(LIMIT, skip);
+
+  if (!ok) return alert(error.message);
+
+  if (posts.length === 0) {
+    if (skip === 0) return appendNoFeed();
+
+    feedListObserver.disconnect();
+    feedList.removeEventListener('intersect', printFeed);
+  } else if (posts.length < LIMIT) {
+    appendPosts(posts);
+    feedListObserver.disconnect();
+    feedList.removeEventListener('intersect', printFeed);
+  } else {
+    skip += LIMIT;
+    appendPosts(posts);
+    feedListObserver.observe(feedList.lastChild);
+  }
+}
+
+function appendNoFeed() {
+  feedList.append(NoFeed());
+}
+
+function appendPosts(posts) {
+  posts.forEach((post) => {
+    const li = document.createElement('li');
+    li.append(PostItem(post, 'profile'));
+    feedList.append(li);
+  });
+}
