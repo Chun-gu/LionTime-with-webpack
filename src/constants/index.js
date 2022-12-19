743486411157376e0b50794853f039222ed53e17
Chun-gu <chungu.yi92@gmail.com>
2022-12-19 23:57:48 +0900

    ✨ 바텀시트에 댓글 관련 기능 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,6 +42,7 @@ export const IMAGE_ERROR = {
 
 export const BUTTON = {
   post: (isMine) => (isMine ? ['update', 'delete'] : ['report']),
+  comment: (isMine) => (isMine ? ['delete'] : ['report']),
 };
 
 export const ACTION = {
