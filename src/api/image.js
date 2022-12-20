816d9b2aee6163a17f40fc102c8183647b8c8f06
Chun-gu <chungu.yi92@gmail.com>
2022-12-20 20:30:12 +0900

    ✨ 다수의 이미지 파일 이름 api

--- src/api/image.js
+++ src/api/image.js
@@ -13,3 +13,26 @@ export async function getImageFileName(image) {
     return { ok: false, error };
   }
 }
+
+export async function getImageFileNames(images) {
+  const formData = new FormData();
+
+  for (const image of images) {
+    if (image instanceof File) formData.append('image', image);
+  }
+
+  try {
+    const response = await api.post('/image/uploadfiles', formData);
+
+    if (!(response instanceof Array)) throw new ResponseError(response);
+
+    const fileNames = response.map((info) => info.filename);
+    const imageFileNames = images
+      .map((image) => (image instanceof File ? fileNames.shift() : image))
+      .join(',');
+
+    return { ok: true, imageFileNames };
+  } catch (error) {
+    return { ok: false, error };
+  }
+}
