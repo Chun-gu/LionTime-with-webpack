16fea7570bcae85bc020d3b53b71c98e554689ee
Chun-gu <chungu.yi92@gmail.com>
2022-12-23 21:10:03 +0900

    ✨ 상품 컴포넌트

---
+++ src/components/ProductItem/index.js
@@ -0,0 +1,39 @@
+import styles from './style.module.css';
+
+import { trimImageURL } from '@utils';
+
+import defaultPostProductImage from '@images/default-post-product-image.webp';
+
+export default function ProductItem(product) {
+  const { id: productId, itemImage, itemName, price } = product;
+
+  const li = document.createElement('li');
+  li.classList.add(styles['product-item'], 'product-item');
+  li.dataset.productId = productId;
+
+  const a = document.createElement('a');
+  a.href = `product?productId=${productId}`;
+
+  const img = document.createElement('img');
+  img.src = trimImageURL(itemImage);
+  img.onerror = ({ target }) => {
+    target.onerror = null;
+    target.src = defaultPostProductImage;
+  };
+  img.classList.add(styles['product-img']);
+
+  const p = document.createElement('p');
+  p.classList.add(styles['product-name']);
+  p.textContent = itemName;
+
+  const span = document.createElement('span');
+  span.classList.add(styles['product-price']);
+  span.textContent = `${price.toLocaleString()}원`;
+
+  a.append(img);
+  a.append(p);
+  a.append(span);
+  li.append(a);
+
+  return li;
+}
