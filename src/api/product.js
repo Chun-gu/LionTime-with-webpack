5a77964810b463d868f3c8e410b43fe9173a5b85
Chun-gu <chungu.yi92@gmail.com>
2023-01-05 22:49:10 +0900

    ✨ 상품 생성 api

--- src/api/product.js
+++ src/api/product.js
@@ -14,3 +14,14 @@ export async function getProducts({ accountname, LIMIT, skip }) {
     return { ok: false, error };
   }
 }
+
+export async function postProduct(product) {
+  try {
+    const response = await api.post('/product', { product });
+
+    if (response.product) return { ok: true, productId: response.product.id };
+    throw new ResponseError(response);
+  } catch (error) {
+    return { ok: false, error };
+  }
+}
