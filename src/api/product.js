8f92bb0fa0bd5b274051552d1ede970c7f8f2f53
Chun-gu <chungu.yi92@gmail.com>
2023-01-06 18:52:43 +0900

    ✨ 상품 디테일 api

--- src/api/product.js
+++ src/api/product.js
@@ -2,6 +2,18 @@ import api from './config';
 
 import { ResponseError } from '@components';
 
+// TODO: fetch 응답 시간 n초 이상이면 abort
+export async function getProduct(productId) {
+  try {
+    const response = await api.get(`/product/detail/${productId}`);
+
+    if (response.product) return { ok: true, product: response.product };
+    throw new ResponseError(response);
+  } catch (error) {
+    return { ok: false, error };
+  }
+}
+
 export async function getProducts({ accountname, LIMIT, skip }) {
   try {
     const response = await api.get(
@@ -20,6 +32,17 @@ export async function postProduct(product) {
     const response = await api.post('/product', { product });
 
     if (response.product) return { ok: true, productId: response.product.id };
+    throw new ResponseError(response);
+  } catch (error) {
+    return { ok: false, error };
+  }
+}
+
+export async function deleteProduct(productId) {
+  try {
+    const response = await api.delete(`/product/${productId}`);
+
+    if (response.status === '200') return { ok: true };
     throw new ResponseError(response);
   } catch (error) {
     return { ok: false, error };
