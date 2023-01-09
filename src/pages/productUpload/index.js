3cdfb22ef0f376a4bf728d335590a3f9c70cec9a
Chun-gu <chungu.yi92@gmail.com>
2023-01-09 18:30:31 +0900

    ✨ 상품 수정 기능 추가

--- src/pages/productUpload/index.js
+++ src/pages/productUpload/index.js
@@ -1,26 +1,67 @@
 import './style.css';
 
-import { postProduct, getImageFileName } from '@api';
-import { PRODUCT_MESSAGE, REGEX } from '@constants';
+import { getImageFileName, getProduct, postProduct, updateProduct } from '@api';
+import { PRODUCT_ERROR, REGEX } from '@constants';
 import { ProductPreview, StatusBar } from '@components';
-import { validateImageFiles } from '@utils';
+import {
+  debounce,
+  getFromQueryString,
+  getImageDataURL,
+  InputValidator,
+  trimImageURL,
+  validateImageFiles,
+} from '@utils';
 
-StatusBar();
+const productId = getFromQueryString('productId');
+const isUpdating = !!productId;
 
 const productForm = document.querySelector('#product-form');
 const previewSection = document.querySelector('#preview-section');
 const imageInput = document.querySelector('#image-input');
 const nameInput = document.querySelector('#name-input');
+const nameErrorMessage = document.querySelector('#name-error-message');
 const priceInput = document.querySelector('#price-input');
+const priceErrorMessage = document.querySelector('#price-error-message');
 const linkInput = document.querySelector('#link-input');
+const linkErrorMessage = document.querySelector('#link-error-message');
+
 const submitButton = document.querySelector('#submit-button');
+
+const nameValidator = new InputValidator().required(PRODUCT_ERROR.nameRequired);
+const priceValidator = new InputValidator()
+  .required(PRODUCT_ERROR.priceRequired)
+  .number(PRODUCT_ERROR.priceShouldBeNumber);
+const linkValidator = new InputValidator()
+  .required(PRODUCT_ERROR.linkRequired)
+  .match(REGEX.url, PRODUCT_ERROR.linkPattern);
+
+let prevImage;
+let prevName;
+let prevPrice;
+let prevLink;
+
+let isImageValid = false;
+let isNameValid = false;
+let isPriceValid = false;
+let isLinkValid = false;
+
+StatusBar();
+
+if (isUpdating) {
+  printPrevProduct(productId);
+  previewSection.append();
+}
 
 imageInput.addEventListener('change', async ({ target: { files } }) => {
   const { isValid, cause } = validateImageFiles(files);
 
   if (isValid) {
-    previewSection.append(await ProductPreview(files[0]));
+    const imageDataURL = await getImageDataURL(files[0]);
+    previewSection.append(ProductPreview(imageDataURL));
   } else alert(cause);
+
+  isImageValid = isValid;
+  toggleSubmitButton();
 });
 
 previewSection.addEventListener('click', ({ target }) => {
@@ -28,33 +69,50 @@ previewSection.addEventListener('click', ({ target }) => {
     const productPreview = document.querySelector('.product-preview');
     productPreview.remove();
     imageInput.value = '';
+
+    isImageValid = false;
+    toggleSubmitButton();
   }
 });
 
-function validateInputs() {
-  return (
-    imageInput.value &&
-    nameInput.value &&
-    priceInput.value &&
-    REGEX.url.test(linkInput.value)
-  );
-}
+nameInput.addEventListener(
+  'input',
+  debounce(({ target: { value: name } }) => {
+    const { isValid, cause } = nameValidator.validate(name);
 
-document.querySelectorAll('input').forEach((input) => {
-  input.addEventListener('input', () => {
-    const isValid = validateInputs();
+    isNameValid = isValid;
+    nameErrorMessage.textContent = cause;
+    toggleSubmitButton();
+  }, 300),
+);
 
-    if (isValid) submitButton.disabled = false;
-    else submitButton.disabled = true;
-  });
-});
+priceInput.addEventListener(
+  'input',
+  debounce(({ target: { value: price } }) => {
+    const { isValid, cause } = priceValidator.validate(price);
+
+    isPriceValid = isValid;
+    priceErrorMessage.textContent = cause;
+
+    toggleSubmitButton();
+  }, 300),
+);
+
+linkInput.addEventListener(
+  'input',
+  debounce(({ target: { value: link } }) => {
+    const { isValid, cause } = linkValidator.validate(link);
+
+    isLinkValid = isValid;
+    linkErrorMessage.textContent = cause;
+    toggleSubmitButton();
+  }, 300),
+);
 
 productForm.addEventListener('submit', async (e) => {
   e.preventDefault();
 
-  const isValid = validateInputs();
-
-  if (!isValid) return;
+  if (!canSubmit()) return;
 
   const imageFileName = await getImageFileName(imageInput.files[0]);
 
@@ -64,11 +122,58 @@ productForm.addEventListener('submit', async (e) => {
     itemName: nameInput.value,
     price: Number(priceInput.value),
     link: linkInput.value,
-    itemImage: imageFileName.filename,
+    itemImage: imageFileName.filename || prevImage,
   };
+  console.log(isUpdating);
+  const result = isUpdating
+    ? await updateProduct(productId, product)
+    : await postProduct(product);
 
-  const { ok, productId, error } = await postProduct(product);
-
-  if (ok) location.href = `product?productId=${productId}`;
-  else alert(error);
+  if (result.ok) location.href = `product?productId=${result.productId}`;
+  else alert(result.error);
 });
+
+async function printPrevProduct(productId) {
+  const { ok, product, error } = await getProduct(productId);
+
+  if (ok) {
+    prevImage = product.itemImage;
+    prevName = product.itemName;
+    prevPrice = product.price;
+    prevLink = product.link;
+
+    isImageValid = true;
+    isNameValid = true;
+    isPriceValid = true;
+    isLinkValid = true;
+
+    previewSection.append(ProductPreview(trimImageURL(prevImage)));
+    nameInput.value = product.itemName;
+    priceInput.value = product.price;
+    linkInput.value = product.link;
+  } else {
+    alert(error);
+    history.back();
+  }
+}
+
+function canSubmit() {
+  const imagePreview = document.querySelector('.product-preview img');
+  const isSameImage = imagePreview?.src === prevImage;
+  const isSameName = nameInput.value === prevName;
+  const isSamePrice = priceInput.value === prevPrice;
+  const isSameLink = linkInput.value === prevLink;
+
+  const areSameValuesWithPrev =
+    isSameImage && isSameName && isSamePrice && isSameLink;
+
+  const areValuesValid =
+    isImageValid && isNameValid && isPriceValid && isLinkValid;
+
+  if (!areSameValuesWithPrev && areValuesValid) return true;
+  return false;
+}
+
+function toggleSubmitButton() {
+  submitButton.disabled = !canSubmit();
+}
