7d57a10f0846bb5f66c491dc36a1b2d3f8087601
Chun-gu <chungu.yi92@gmail.com>
2023-01-14 21:50:30 +0900

    ♻️ navigate에 뒤로 가는 옵션값 추가

--- src/utils/navigate.js
+++ src/utils/navigate.js
@@ -1,4 +1,12 @@
-export default function navigate(url, option = { replace: false }) {
-  if (option.replace) location.replace(url);
-  else location.assign(url);
+export default function navigate({ to = '', goBack = false, replace = false }) {
+  let destination = to;
+
+  if (goBack) {
+    const referrer = document.referrer;
+    const prevPageURL = sessionStorage.getItem('prevPageURL');
+    destination = referrer || prevPageURL || '/';
+  }
+
+  if (replace) location.replace(destination);
+  else location.assign(destination);
 }
